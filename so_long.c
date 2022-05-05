7177f88bff04ba403244495613aca52cb4f206e5
xDec0de_ <xdec0deyt@gmail.com>
2022-05-05 12:37:32 +0200

    Variable rename & header fix

--- so_long.c
+++ so_long.c
@@ -6,25 +6,25 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 10:20:33 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/05 12:26:55 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/05 12:35:59 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	draw_pixel(t_data *data, int x, int y, int color)
+void	draw_pixel(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
 
-int	close(int keycode, t_vars *vars)
+int	close_win(int keycode, t_mlx *mlx)
 {
 	if (keycode == 53)
 	{
-		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_window(mlx->mlx, mlx->win);
 		exit(0);
 	}
 	return (0);
@@ -32,15 +32,15 @@ int	close(int keycode, t_vars *vars)
 
 int	main(void)
 {
-	t_vars	vars;
-	t_data	img;
+	t_mlx	mlx;
+	t_img	img;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 120, 120, "so_long");
-	img.img = mlx_new_image(vars.mlx, 120, 120);
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 120, 120, "so_long");
+	img.img = mlx_new_image(mlx.mlx, 120, 120);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_loop(vars.mlx);
+	mlx_hook(mlx.win, 2, 1L << 0, close, &mlx);
+	mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
+	mlx_loop(mlx.mlx);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:23:44 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/05 12:26:20 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/05 12:36:17 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@
 # include <stdlib.h>
 #include <stdio.h> // debug
 
-void	draw_pixel(t_data *data, int x, int y, int color);
-
-int		close(int keycode, t_vars *vars);
-
 typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}				t_data;
+}				t_img;
 
 typedef struct s_vars {
 	void	*mlx;
 	void	*win;
-}				t_vars;
+}				t_mlx;
+
+void	draw_pixel(t_img *img, int x, int y, int color);
+
+int		close_win(int keycode, t_mlx *mlx);
 
 #endif
