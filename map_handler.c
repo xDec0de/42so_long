4ba15d48fce7e14689bb17c5df7580df52220d86
danimart <danimart@student.42madrid.com>
2022-05-08 20:37:03 +0200

    Texture print test

--- map_handler.c
+++ map_handler.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 13:13:58 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/08 16:33:17 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/08 20:32:21 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,32 @@
 int	draw_map(t_map map)
 {
 	t_mlx	mlx;
-	//t_img	img;
+	t_img	img;
+	int		x;
+	int		y;
+	int		d;
 
+	d = 16;
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, map.length * 16, map.height * 16, "so_long");
-	//img.img = mlx_new_image(mlx.mlx, 120, 120);
-	//img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	img.img = mlx_xpm_file_to_image(mlx.mlx, "./img/wall.xpm", &d, &d);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	mlx_key_hook(mlx.win, handle_key, &mlx);
 	mlx_hook(mlx.win, 17, 0L, user_end, &mlx);
-	//mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
+	x = 0;
+	y = 0;
+	while(x < map.length) {
+		y = 0;
+		while(y < map.length) {
+			if (x % 2 == 0)
+				img.img = mlx_xpm_file_to_image(mlx.mlx, "./img/wall.xpm", &d, &d);
+			else
+				img.img = mlx_xpm_file_to_image(mlx.mlx, "./img/key.xpm", &d, &d);
+			mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, x * 16, y * 16);
+			y++;
+		}
+		x++;
+	}
 	mlx_loop(mlx.mlx);
 	return (map_free(map.arr, map.height, 0));
 }
