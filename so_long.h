50e9898e9599ec992e93c42b13699bcf6dbd92d3
xDec0de <xdec0deyt@gmail.com>
2025-03-07 14:34:16 +0100

    Added newline characters on all messages

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:23:44 by daniema3          #+#    #+#             */
-/*   Updated: 2025/03/06 14:06:51 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/03/07 14:04:06 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,47 +22,47 @@
 # define BUFFER_SIZE 1024
 
 # define INPUT_ERR "Error\n\e[0;31mInvalid input, usage\e[1;30m: \
- \e[0;33m./so_long map_name.ber\e[1;30m.\e[0m"
+ \e[0;33m./so_long map_name.ber\e[1;30m.\e[0m\n"
 # define INPUT_ERRC 1
 
 # define MAP_EXTENSION_ERR "Error\n\e[0;31mInvalid map format, maps must have \
-the \e[0;33m.ber \e[0;31mIextension\e[1;30m.\e[0m"
+the \e[0;33m.ber \e[0;31mIextension\e[1;30m.\e[0m\n"
 # define MAP_EXTENSION_ERRC 2
 
-# define MALLOC_ERR "Error\n\e[0;31mMalloc failed\e[1;30m.\e[0m"
+# define MALLOC_ERR "Error\n\e[0;31mMalloc failed\e[1;30m.\e[0m\n"
 # define MALLOC_ERRC 3
 
 # define MAP_OPEN_ERR "Error\n\e[0;31mMap file could not be opened, \
-wrong name maybe?\e[0m"
+wrong name maybe?\e[0m\n"
 # define MAP_OPEN_ERRC 4
 
 # define READ_ERR "Error\n\e[0;31mRead failed\e[1;30m.\e[0m"
 # define READ_ERRC 4
 
 # define MAP_EMPTY_ERR "Error\n\e[0;31mInvalid map content\e[1;30m: \
-\e[0;31mMap is empty\e[1;30m.\e[0m"
+\e[0;31mMap is empty\e[1;30m.\e[0m\n"
 
 # define MAP_CONTENT_ERR "Error\n\e[0;31mInvalid map content\e[1;30m: \
-\e[0;31mMap contains illegal characters\e[1;30m.\e[0m"
+\e[0;31mMap contains illegal characters\e[1;30m.\e[0m\n"
 
 # define MAP_LEN_ERR "Error\n\e[0;31mInvalid map content\e[1;30m: \
-\e[0;31mMap is not a rectangle, some lines are... So_long\e[1;30m.\e[0m"
+\e[0;31mMap is not a rectangle, some lines are... So_long\e[1;30m.\e[0m\n"
 
 # define MAP_SIZE_ERR "Error\n\e[0;31mInvalid map content\e[1;30m:\
- \e[0;31mMap is too big.\e[1;30m.\e[0m"
+ \e[0;31mMap is too big.\e[1;30m.\e[0m\n"
 
 # define MAP_STRCT_ERR "Error\n\e[0;31mInvalid map content\e[1;30m: \
-\e[0;31mMap must be closed with walls\e[1;30m.\e[0m"
+\e[0;31mMap must be closed with walls\e[1;30m.\e[0m\n"
 
 # define MAP_OBJ_ERR "Error\n\e[0;31mInvalid map content\e[1;30m: \
 \e[0;31mMissing or too many objects. \e[1;30m(\e[0;31mMap\
  must have only one exit, one player and at least one\
- collectable\e[1;30m).\e[0m"
+ collectable\e[1;30m).\e[0m\n"
 
 # define MAP_IMPOSSIBLE "Error\n\e[0;31mInvalid map content\e[1;30m: \
-\e[0;31mMap is impossible to win, please nerf\e[1;30m.\e[0m"
+\e[0;31mMap is impossible to win, please nerf\e[1;30m.\e[0m\n"
 
-# define MLX_ERR "Error\n\e[0;31mMlx error\e[1;30m.\e[0m"
+# define MLX_ERR "Error\n\e[0;31mMlx error\e[1;30m.\e[0m\n"
 
 # define WINDOW_TITLE "so_long"
 
